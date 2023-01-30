a730cdb618f8155c877be656fffd06f3405b4c9e
janusuresh <121850465+janusuresh@users.noreply.github.com>
2023-01-30 10:56:53 +0530

    Update databaseConnection.php

--- PHP/DATABASE/databaseConnection.php
+++ PHP/DATABASE/databaseConnection.php
@@ -1,5 +1,5 @@
 <!-- database-connect :: start -->
 <?php
-    $link = @mysqli_connect("localhost","mykec","kongu@2040","mykec") or die("ERROR:Unable to connect".mysqli_connect_error());
+    $link = @mysqli_connect("localhost","<username>","<password>","mykec") or die("ERROR:Unable to connect".mysqli_connect_error());
 ?>
 <!-- database-connect :: end -->
