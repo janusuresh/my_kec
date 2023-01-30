3f9462bf76ef51023a57823f0e6538f0ea75c22d
janusuresh <121850465+janusuresh@users.noreply.github.com>
2023-01-30 10:56:24 +0530

    Delete appapi directory

--- appapi/staff/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-$con=new mysqli("localhost","mykec","kongu@2040","mykec");
-?>
--- appapi/student/launchpdf.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'config.php';
-header("Access-Control-Allow-Origin: *");
-$rollNo = $_POST['rollNo'];
-$sem = $_POST['sem'];
-
-
-?>
